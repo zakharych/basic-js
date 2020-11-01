e11fa42cd8bb67df0908c67aa4e3b51c18b7bf4e
zakharych <alexzlkl@yandex.ru>
2020-11-01 18:48:03 +0300

    feat: towel

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ require('./test/dream-team.test');  /////   ОК
 require('./test/extended-repeater.test');
 require('./test/what-season.test');/////   почти
 require('./test/vigenere-cipher.test');
-require('./test/hanoi-tower.test');
+require('./test/hanoi-tower.test'); ///// ок
 require('./test/transform-array.test');
 require('./test/simple-chain.test');
 require('./test/recursive-depth.test');
