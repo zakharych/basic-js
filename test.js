4f2ae2bf7f5982342220a814605891b43b38aae0
zakharych <alexzlk@yandex.ru>
2021-02-26 13:25:37 +0300

    feat: transform task ready

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
-// require('./test/count-cats.test');  /////   ОК
-// require('./test/carbon-dating.test');
-// require('./test/dream-team.test');  /////   ОК
-// require('./test/extended-repeater.test');  /////   ОК
-// require('./test/what-season.test');/////   почти
-// require('./test/vigenere-cipher.test');
-// require('./test/hanoi-tower.test'); ///// ок
-require('./test/transform-array.test');
-// require('./test/simple-chain.test');
-// require('./test/recursive-depth.test');
+require('./test/count-cats.test');  /////   ОК
+require('./test/carbon-dating.test');
+require('./test/dream-team.test');  /////   ОК
+require('./test/extended-repeater.test');  /////   ОК
+require('./test/what-season.test');/////   почти
+require('./test/vigenere-cipher.test');
+require('./test/hanoi-tower.test'); ///// ок
+require('./test/transform-array.test');///// ок
+require('./test/simple-chain.test');
+require('./test/recursive-depth.test');
