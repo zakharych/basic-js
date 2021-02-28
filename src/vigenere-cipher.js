0f42fd49a62d1d8de5eecd36082e2d5bc9d953c7
Alex <alexzlkl@yandex.ru>
2021-02-28 18:07:54 +0300

    feat: vigenere done

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,64 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(mode = true) {
+    this.mode = mode;
+  }
+  encrypt(message, key) {
+    let decryptString = "";
+    for (let i = 0, j = 0; i < message.length; i++) {
+      let currentLetter = message[i];
+      let currentLetterCode = currentLetter.charCodeAt();
+
+      if (
+        (currentLetterCode >= 97 && currentLetterCode <= 122) ||
+        (currentLetterCode >= 65 && currentLetterCode <= 90)
+      ) {
+        let encryptSimbol =
+          (currentLetter.toUpperCase().charCodeAt() -
+            65 +
+            (key[j % key.length].toUpperCase().charCodeAt() - 65)) %
+          26;
+        decryptString += String.fromCharCode(encryptSimbol + 65);
+        j++;
+      } else {
+        decryptString += currentLetter;
+      }
+    }
+    if (this.mode === false ) {
+      return decryptString.split("").reverse().join("");
+      
+    }
+    return decryptString;
+  }
+  decrypt(encryptedMessage, key) {
+    let decryptedString = "";
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      let currentLetter = encryptedMessage[i];
+      let currentLetterCode = currentLetter.charCodeAt();
+
+      if (
+        (currentLetterCode >= 97 && currentLetterCode <= 122) ||
+        (currentLetterCode >= 65 && currentLetterCode <= 90)
+      ) {
+        let decryptSimbol =
+          (currentLetter.toUpperCase().charCodeAt() -
+            65 +
+            26 -
+            (key[j % key.length].toUpperCase().charCodeAt() - 65)) %
+          26;
+          decryptedString += String.fromCharCode(decryptSimbol + 65);
+        j++;
+      } else {
+        decryptedString += currentLetter;
+      }
+    }
+    
+    if (this.mode === false ) {
+      return decryptedString.split("").reverse().join("");
+      
+    }
+    return decryptedString;
   }
 }
 
