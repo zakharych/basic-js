148724ff1de1500c1504d4ab8e9ee6dc0e76b197
zakharych <alexzlkl@yandex.ru>
2021-03-01 19:12:57 +0300

    feat: depth ready

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,14 +1,14 @@
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = class DepthCalculator {
-class DepthCalculator {
+module.exports = class DepthCalculator {
+// class DepthCalculator {
   calculateDepth(arr) {
-    let counter = 0;
     let depth = 0;
     arr.forEach((element) => {
+      let counter = 0;
       if (Array.isArray(element)) {
         counter += this.calculateDepth(element);
-      } 
+      }
       if (counter > depth) {
         depth = counter;
       }
@@ -16,10 +16,3 @@ class DepthCalculator {
     return depth + 1;
   }
 }
-
-const instance = new DepthCalculator();
-const calculateDepth = instance.calculateDepth.bind(instance);
-
-// console.log(calculateDepth([1, [8, [[]]], 2, 3, [8, []], 4, 5, ['6575',['adas', ['dfg', [0]]]]]));
-
-console.log(calculateDepth([[1], [2, [1, []]]]));
