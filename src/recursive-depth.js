91b0a38a03f0149e0be25edbef1d3d120e02bafa
zakharych <alexzlk@yandex.ru>
2021-03-01 12:38:09 +0300

    feat: depth task without one check

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,42 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    console.log(arr);
+    let deepth = 0;
+    let counter = 0;
+    function checkArray(arr) {
+      arr.forEach((element) => {
+        if (Array.isArray(element)) {
+          counter++;
+          checkArray(element);
+        }
+      });
+      if (counter > deepth) {
+        deepth = counter;
+        counter = 0;
+      } else {
+        counter = 0;
+      }
+    }
+    checkArray(arr);
+
+    return deepth + 1;
   }
 };
+
+// const instance = new DepthCalculator();
+// const calculateDepth = instance.calculateDepth.bind(instance);
+
+// calculateDepth([
+//   1,
+//   [8, [[]]],
+//   [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]], []]]], []]]]]]]]], []]]], []]]]]]]]]],
+//   2,
+//   3,
+//   [8, [[[[[[[[[[[[[[]]]]]]]]]]]]]]],
+//   [8, [[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]],
+//   4,
+//   5,
+//   ["6575", ["adas", ["dfg", [0]]]],
+// ]);
